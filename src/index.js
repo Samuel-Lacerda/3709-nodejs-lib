65eb151b902389c37dcfe0354223a1b9c246af17
Samuel Lacerda <127802525+Samuel-Lacerda@users.noreply.github.com>
2024-08-16 05:46:52 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,9 +4,28 @@ const caminhoArquivo = process.argv
 const link = caminhoArquivo[2]
 
 fs.readFile(link, 'utf-8', (erro, texto) => {
-    verificaPalavrasDuplicadas(texto)
+    //verificaPalavrasDuplicadas(texto)
+    quebraEmParagrafos(texto)
 })
 
-function verificaPalavrasDuplicadas(texto){
+// criar um array com as palavras
+// contar as ocorrências
+// montar um objeto com o resultado
 
+function quebraEmParagrafos(texto){
+    const paragrafos = texto.toLowerCase().split('\n')
+    const contagem = paragrafos.map((paragrafo) => {
+        return verificaPalavrasDuplicadas(paragrafo)
+    })
+    console.log(contagem)
+}
+
+function verificaPalavrasDuplicadas(texto){
+    const listaPalavras = texto.split(' ')
+    const resultado = {}
+    // objeto[propriedade] = valor
+    listaPalavras.forEach(palavra => {
+        resultado[palavra] = (resultado[palavra] || 0) + 1
+    })
+    return resultado
 }
