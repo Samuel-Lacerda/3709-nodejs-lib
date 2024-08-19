113a030ecc8725decdd27a975be85f4c2304e92d
Samuel Lacerda <127802525+Samuel-Lacerda@users.noreply.github.com>
2024-08-19 06:22:08 -0300

    update

---
+++ src/cli.js
@@ -0,0 +1,20 @@
+import fs from 'fs'
+import trataErros from './erros/funcoesErro.js'
+import { contaPalavras } from './index.js'
+
+const fs = require('fs')
+const trataErros = require('./erros/funcoesErro')
+
+
+const caminhoArquivo = process.argv
+const link = caminhoArquivo[2]
+
+
+fs.readFile(link, 'utf-8', (erro, texto) => {
+    try{
+      if(erro) throw erro
+      contaPalavras(texto)
+    } catch(erro){
+      trataErros(erro)
+    }
+  })
